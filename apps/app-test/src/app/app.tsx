8850e712608561ddadce641d4b3079f0b604712c
Dmitry Golysh <disist@users.noreply.github.com>
2023-09-14 12:41:31 +0300

    test(app): Test updates commit (#3)
    Co-authored-by: Dmytro_Golysh <dmytro_golysh@ls2.io>

--- apps/app-test/src/app/app.tsx
+++ apps/app-test/src/app/app.tsx
@@ -9,7 +9,7 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="app-test" />
+      <NxWelcome title="app-test-updates" />
     </StyledApp>
   );
 }
