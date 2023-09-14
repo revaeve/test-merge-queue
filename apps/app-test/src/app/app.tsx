85fb78aa73181a99609e8b4c9017262f24d1a9ec
Dmitry Golysh <disist@users.noreply.github.com>
2023-09-14 13:09:35 +0300

    test PR 1 (#6)
    Co-authored-by: Dmytro_Golysh <dmytro_golysh@ls2.io>

--- apps/app-test/src/app/app.tsx
+++ apps/app-test/src/app/app.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import NxWelcome from './nx-welcome';
 
 const StyledApp = styled.div`
-  // Your style here updates-new
+  // Your style here updates-new-1
 `;
 
 export function App() {
