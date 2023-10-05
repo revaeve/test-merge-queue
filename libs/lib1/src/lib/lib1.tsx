8e13a24ea7b5a41dc46e6f410e2838cb62a82c8c
Dmytro_Golysh <dmytro_golysh@ls2.io>
2023-10-05 19:09:05 +0300

    Update lib1

--- libs/lib1/src/lib/lib1.tsx
+++ libs/lib1/src/lib/lib1.tsx
@@ -10,7 +10,7 @@ const StyledLib1 = styled.div`
 export function Lib1(props: Lib1Props) {
   return (
     <StyledLib1>
-      <h1>Welcome to Lib1! Updated 90</h1>
+      <h1>Welcome to Lib1! Updated 95</h1>
     </StyledLib1>
   );
 }
