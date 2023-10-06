e8ca7f5aef469257dbf8acd4afc25d3fa333cd9a
Dmytro_Golysh <dmytro_golysh@ls2.io>
2023-10-06 13:25:02 +0300

    Add needed permission to push commits. Add version to changelog

--- scripts/update-changelogs.js
+++ scripts/update-changelogs.js
@@ -28,25 +28,12 @@ try {
   affectedApps.forEach((appName) => {
     const fullPath = path.join(__dirname, '../apps/', appName);
 
-    // Add commit message to changelog
-    const changelogPath = path.join(fullPath, 'CHANGELOG.md');
-    if (fs.existsSync(changelogPath)) {
-      let changelogContent = fs.readFileSync(changelogPath, 'utf8');
-      changelogContent += `\n---\n\n${COMMIT_MESSAGE}\n`;
-      fs.writeFileSync(changelogPath, changelogContent, 'utf8');
-      console.log(`Updated ${changelogPath}`);
-    } else {
-      console.log(
-        `No CHANGELOG.md found in ${fullPath}. Creating one for you.`
-      );
-      fs.writeFileSync(changelogPath, `${COMMIT_MESSAGE}\n`, 'utf8');
-    }
-
     // Increase version for affected app
+    let version;
     const packageJsonPath = path.join(fullPath, 'package.json');
     if (fs.existsSync(packageJsonPath)) {
       const packageJson = require(packageJsonPath);
-      const version = packageJson.version;
+      version = packageJson.version;
       const versionParts = version.split('.');
       const newVersion = `${versionParts[0]}.${versionParts[1]}.${
         parseInt(versionParts[2]) + 1
@@ -62,11 +49,27 @@ try {
       console.log(
         `No package.json found in ${fullPath}. Creating one for you.`
       );
+      version = '0.0.1';
       fs.writeFileSync(
         packageJsonPath,
-        JSON.stringify({ version: '0.0.1' }, null, 2),
+        JSON.stringify({ version }, null, 2),
         'utf8'
       );
+    }
+
+    // Add commit message to changelog
+    const changelogPath = path.join(fullPath, 'CHANGELOG.md');
+    const updateMessage = `${version}: ${COMMIT_MESSAGE}`;
+    if (fs.existsSync(changelogPath)) {
+      let changelogContent = fs.readFileSync(changelogPath, 'utf8');
+      changelogContent += `\n---\n\n${updateMessage}\n`;
+      fs.writeFileSync(changelogPath, changelogContent, 'utf8');
+      console.log(`Updated ${changelogPath}`);
+    } else {
+      console.log(
+        `No CHANGELOG.md found in ${fullPath}. Creating one for you.`
+      );
+      fs.writeFileSync(changelogPath, `${updateMessage}\n`, 'utf8');
     }
   });
 } catch (e) {
